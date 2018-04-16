b48ff4851f66de17b2f5f01e6f4d950d11b7d2ba
kamilkhan@rediffmail.com <donkhan>
2018-04-16 10:19:41 +0530

    Body is more or less created

--- app/SOAPTestCode.java
+++ app/SOAPTestCode.java
@@ -14,7 +14,7 @@ public class SOAPTestCode {
             SOAPEnvelope envelope = part.getEnvelope();
             SOAPHeader header = envelope.getHeader();
 
-
+            /*
             SOAPHeaderElement security = (SOAPHeaderElement)header.addChildElement("Security", "wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
             security.setMustUnderstand(true);
 
@@ -25,16 +25,14 @@ public class SOAPTestCode {
 
             SOAPElement passWord = usernameToken.addChildElement("Password","wsse");
             passWord.addTextNode("PASSWORD");
-            SOAPBody body = envelope.getBody();
+            */
 
+            SOAPBody body = envelope.getBody();
             QName bodyName = new QName("", "submitDocument", "v1");
             SOAPBodyElement bodyElement = body.addBodyElement(bodyName);
-
-            QName name = new QName("parameters");
-            SOAPElement parameters = bodyElement.addChildElement(name);
-            QName q = new QName("http://customer.endpoint.earthport.com/api/merchant/v1/services/createOrUpdateUser",
-                    "createOrUpdateUser","ns2");
-            SOAPElement co = parameters.addChildElement(q);
+            SOAPElement parameters = bodyElement.addChildElement(new QName("parameters"));
+            SOAPElement co = parameters.addChildElement(new QName("http://customer.endpoint.earthport.com/api/merchant/v1/services/createOrUpdateUser",
+                    "createOrUpdateUser","ns2"));
 
             co.setAttribute("xmlns:ns","http://customer.endpoint.earthport.com/api/merchant/v2/components/core");
             co.setAttribute("xmlns:ns3","http://customer.endpoint.earthport.com/api/merchant/v2/components/identityBase");
@@ -42,14 +40,21 @@ public class SOAPTestCode {
 
             SOAPElement mui = parameters.addChildElement(new QName("merchantUserIdentity","merchantUserIdentity","ns2"));
             mui.setTextContent("test_user343");
-            /*
-            SOAPElement currency = parameters.addChildElement(new QName("ns2","accountCurrency"));
-            currency.setTextContent("GBP");
-            SOAPElement payerIdentity = parameters.addChildElement(new QName("ns2","accountCurrency"));
 
-            SOAPElement payerIndividiualIdentity = payerIdentity.addChildElement(new QName("ns3",""));
-            System.out.println(co);
-            */
+            SOAPElement currency = parameters.addChildElement(new QName("accountCurrency","accountCurrency","ns2"));
+            currency.setTextContent("GBP");
+
+            SOAPElement payerIdentity = parameters.addChildElement(new QName("payerIdentity","payerIdentity","ns2"));
+            SOAPElement payerIndividiualIdentity = payerIdentity.addChildElement(new QName("payerIndividualIdentity",
+                    "payerIndividualIdentity","ns3"));
+            SOAPElement name = payerIndividiualIdentity.addChildElement(new QName("name","name","ns3"));
+            name.addChildElement(new QName("givenNames","givenNames","ns3")).setTextContent("Donald");
+            name.addChildElement(new QName("familyName","familyName","ns3")).setTextContent("Trump");
+
+            SOAPElement address = payerIndividiualIdentity.addChildElement(new QName("address","address","ns3"));
+            address.addChildElement(new QName("addressLine1","addressLine1","ns3")).setTextContent("13th Cross");
+
+
             soapMsg.writeTo(System.out);
         }catch(Throwable t){
             t.printStackTrace();
